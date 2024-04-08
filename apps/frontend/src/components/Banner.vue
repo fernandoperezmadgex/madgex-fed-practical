6fb82a95aea454191ed6e51149f968800be6bb88
fernandoperezmadgex <fperez@wiley.com>
2024-04-08 16:32:56 +0100

    Remove margin top for banner in desktop size

--- apps/frontend/src/components/Banner.vue
+++ apps/frontend/src/components/Banner.vue
@@ -1,7 +1,7 @@
 <script setup></script>
 
 <template>
-  <div class="border-brandColour2 bg-brandColour4 mt-5 border py-[25px] text-center">
+  <div class="border-brandColour2 bg-brandColour4 border py-[25px] text-center">
     <span>Ad</span>
   </div>
 </template>
--- apps/frontend/src/components/layout/MainHeader.vue
+++ apps/frontend/src/components/layout/MainHeader.vue
@@ -4,7 +4,7 @@ import Banner from '@/components/Banner.vue';
 
 <template>
   <header>
-    <Banner class="mx-auto block w-[90%] lg:hidden" />
+    <Banner class="mx-auto mt-5 block w-[90%] lg:hidden" />
     <div class="mx-auto mb-4 mt-8 gap-5 text-center md:mx-auto md:w-[90%] lg:mx-auto lg:max-w-[1400px]">
       <h1 class="text-xl font-bold md:text-left md:text-2xl">Browse jobs</h1>
     </div>
